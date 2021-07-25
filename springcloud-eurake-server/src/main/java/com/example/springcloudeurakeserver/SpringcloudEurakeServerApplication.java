60fd96666fdbe2b6fd53be75170a43b24956e9dd
csl <Censenlin@163.com>
2021-07-25 21:23:05 +0800

    第一次提交

---
+++ springcloud-eurake-server/src/main/java/com/example/springcloudeurakeserver/SpringcloudEurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeurakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEurakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurakeServerApplication.class, args);
+    }
+
+}
---
+++ springcloud-eurake-server/src/test/java/com/example/springcloudeurakeserver/SpringcloudEurakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeurakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
